9ea24d311263babcf881a61e401097abc9dba18d
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-07-27 00:10:59 +0530

    Update Q7.R

--- Q7.R
+++ Q7.R
@@ -1,1 +1,18 @@
+# Read the content of the file:
+file_path <- "table.txt"
+contents <- read.delim(file_path, header = TRUE, sep = "\t")
+print(contents)
 
+# Find the missing values:
+print("Missing values:")
+missing_values <- subset(contents, Age == '--' | Height == '--' | Weight == '--' | Age == '*' | Height == '**' | Weight == '**')
+print(missing_values)
+
+# Replace the missing values in column 4 with the average of the column values:
+numeric_values <- as.numeric(as.character(contents$Weight))
+average <- mean(numeric_values, na.rm = TRUE)
+
+contents$Weight[is.na(numeric_values)] <- average
+
+print("Modified contents:")
+print(contents)
