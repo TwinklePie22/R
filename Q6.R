90ccb625ee855a8a80821a8dd64ef92d66a8d3d2
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-07-27 00:03:14 +0530

    Update Q6.R

--- Q6.R
+++ Q6.R
@@ -1,8 +1,25 @@
-# Here we have created sample data frame
+# Define the function to replace a value with NA in a data frame
+replaceValueWithNA <- function(df, value_to_replace) {
+  # Convert the value to replace to NA if it matches any element in the data frame
+  df[df == value_to_replace] <- NA
+  
+  return(df)
+}
+
+# Example data frame
 df <- data.frame(
-col1 = c(1, 2, 3, 4),
-col2 = c("a", "b", "c", "d"),
-col3 = c(5, 6, 7, 8)
+  Name = c("John", "Alice", "Bob", "Emily"),
+  Age = c(25, 30, 22, 25),
+  Grade = c("A", "B", "C", "A")
 )
+
 # Print the original data frame
-print(df);
+print("Original data frame:")
+print(df)
+
+# Replace all occurrences of "A" with NA in the "Grade" column
+modified_df <- replaceValueWithNA(df, "A")
+
+# Print the modified data frame
+print("\nData frame with 'A' replaced with NA:")
+print(modified_df)
