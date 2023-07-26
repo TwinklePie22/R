e5faca6cb1a3cb357bc6732b41b4f1621afa3884
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-07-26 23:50:15 +0530

    Create Q6.R

---
+++ Q6.R
@@ -0,0 +1,8 @@
+# Here we have created sample data frame
+df <- data.frame(
+col1 = c(1, 2, 3, 4),
+col2 = c("a", "b", "c", "d"),
+col3 = c(5, 6, 7, 8)
+)
+# Print the original data frame
+print(df);
