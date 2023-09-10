8a3be7b866c010b8f25507b1e890071d328f1a88
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-09-10 19:14:36 +0530

    Update Q1.R

--- Q1.R
+++ Q1.R
@@ -1,6 +1,7 @@
 # Create vectors A and B with multiple elements
 A <- c(1, 2, 3, 4, 5)
 B <- c(6, 7, 8, 9, 10)
+
 # Arithmetic operations on vectors A and B
 sum_A_B <- A + B # Addition
 diff_A_B <- A - B # Subtraction
@@ -13,11 +14,13 @@ cat("Difference of A and B:", diff_A_B, "\n")
 cat("Product of A and B:", prod_A_B, "\n")
 cat("Division of A and B:", div_A_B, "\n")
 cat("Exponentiation of A and B:", pow_A_B, "\n")
+
 # Apply rep(), paste(), and cumprod() functions on vector A
 rep_A <- rep(A, times = 2) # Repeat A twice
-paste_A <- paste("A:", A, sep = " ") # Concatenate "A:" with each element of A
+paste_A <- paste(A, sep = " ") # Concatenate "A:" with each element of A
 cumprod_A <- cumprod(A) # Cumulative product of A
+
 # Print the results
 cat("Repeated A:", rep_A, "\n")
-cat("Concatenated A:", paste_A, "\n")
+cat("Paste A:", paste_A, "\n")
 cat("Cumulative product of A:", cumprod_A, "\n")
