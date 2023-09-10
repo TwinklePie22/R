a181908992c7ca61792269a3bbcd89bfefb75a8f
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-09-10 19:36:47 +0530

    Update Q2.R

--- Q2.R
+++ Q2.R
@@ -1,26 +1,29 @@
-#2a 
+#2a
+# Create the initial data frame
 Name <- c("Ram", "Alwin", "Billy", "Amera", "Olive", "Dora")
 Age <- c(30, 35, 22, 16, 42, 59)
 Height <- c(177, 164, 155, 180, 124, 150)
 Weight <- c(57, 48, 45, 60, 52, 55)
 Gender <- as.factor(c("M", "F", "M", "F", "F", "F"))
 df <- data.frame(Name, Age, Height, Weight, Gender)
+df
 # Invert gender for all individuals
 levels(df$Gender) <- c("M", "F")
 df$Gender <- as.factor(df$Gender)
 df
 
 #2b
-Name <- c("Ram", "Alwin", "Billy", "Amera", "Olive", "Dora")
-Age <- c(30, 35, 22, 16, 42, 59)
-Height <- c(177, 164, 155, 180, 124, 150)
-Weight <- c(57, 48, 45, 60, 52, 55)
-Gender <- as.factor(c("M", "F", "M", "F", "F", "F"))
-df <- data.frame(Name, Age, Height, Weight, Gender)
-# Invert gender for all individuals
-levels(df$Gender) <- c("M", "F")
-df$Gender <- as.factor(df$Gender)
-df
+# # Create the initial data frame
+# Name <- c("Ram", "Alwin", "Billy", "Amera", "Olive", "Dora")
+# Age <- c(30, 35, 22, 16, 42, 59)
+# Height <- c(177, 164, 155, 180, 124, 150)
+# Weight <- c(57, 48, 45, 60, 52, 55)
+# Gender <- as.character(c("M", "F", "M", "F", "F", "F"))
+# df <- data.frame(Name, Age, Height, Weight, Gender, stringsAsFactors = FALSE)
+# df
+# # Invert gender for all individuals
+# df$Gender <- ifelse(df$Gender == "M", "F", "M")
+# df
 # Create the new data frame
 Working <- c("Y", "N", "Y", "N", "Y", "N")
 df2 <- data.frame(Working, stringsAsFactors = FALSE)
@@ -28,3 +31,11 @@ df2 <- data.frame(Working, stringsAsFactors = FALSE)
 df <- cbind(df, df2)
 # Print the modified data frame
 df
+# Print the number of rows and columns
+cat("Number of rows:", nrow(df), "\n")
+cat("Number of columns:", ncol(df), "\n")
+# Print the class of data in each column
+cat("Class of data in each column:\n")
+for (col in names(df)) {
+  cat(paste(col, ":", class(df[[col]]), "\n"))
+}
