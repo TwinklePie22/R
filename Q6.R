c5a2860b1613f3dca110083d40860bd1bf804fd0
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-09-10 19:55:38 +0530

    Update Q6.R

--- Q6.R
+++ Q6.R
@@ -2,17 +2,14 @@
 replaceValueWithNA <- function(df, value_to_replace) {
   # Convert the value to replace to NA if it matches any element in the data frame
   df[df == value_to_replace] <- NA
-  
   return(df)
 }
-
 # Example data frame
 df <- data.frame(
   Name = c("John", "Alice", "Bob", "Emily"),
   Age = c(25, 30, 22, 25),
   Grade = c("A", "B", "C", "A")
 )
-
 # Print the original data frame
 print("Original data frame:")
 print(df)
