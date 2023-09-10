921453eba17ed6052298a185e014e22b80314c61
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-09-10 22:30:20 +0530

    Update Q10.R

--- Q10.R
+++ Q10.R
@@ -1,26 +1,35 @@
 # Load the mtcars dataset
 data(mtcars)
-
+#!0a
 # Select four different continuous variables from the mtcars dataset
 selected_vars <- c("mpg", "disp", "hp", "wt")
 
-# Plot with lines and points for different variables with different colors
-par(mfrow = c(2, 2))  # Set up a 2x2 grid for subplots
-for (var in selected_vars) {
-  plot(mtcars[, var], type = "b", pch = 16, col = "blue", lwd = 2,
-       main = paste("Line and Points Plot of", var),
-       xlab = "Index", ylab = var)
-}
+# Set up a single plot
+par(mfrow = c(1, 1))
 
-# Choose one variable from each and highlight it with different color
-highlight_vars <- c("mpg", "disp", "hp", "wt")
+# Define colors and line widths for each variable
+colors <- c("blue", "green", "red", "orange")
+line_widths <- c(1, 1, 1, 2)  # Adjust line widths as needed
 
-par(mfrow = c(1, 1))  # Reset the plotting layout to a single plot
-for (i in seq_along(selected_vars)) {
+# Create a plot for each selected variable with one variable highlighted
+for (i in seq_along(selected_vars)) {  # Use seq_along instead of 1:length
   var <- selected_vars[i]
-  highlight_var <- highlight_vars[i]
-  plot(mtcars[, var], type = "b", pch = 16, col = ifelse(var == highlight_var, "red", "blue"),
-       lwd = ifelse(var == highlight_var, 2, 1),
-       main = paste("Line and Points Plot of", var),
-       xlab = "Index", ylab = var)
+  col <- colors[i]
+  lwd <- line_widths[i]
+  
+  plot(mtcars[, var], type = "n", xlab = "Index", ylab = var, main = paste("Line and Points Plot of", var))
+  lines(mtcars[, var], col = col, lwd = lwd)
+  points(mtcars[, var], col = col, pch = 16)
+}
+#10b
+# Choose one variable from each group and highlight it with a different color
+highlight_vars <- c("mpg", "disp", "hp", "wt")
+highlight_colors <- c("red", "green", "blue", "orange")
+
+for (i in seq_along(highlight_vars)) {  # Use seq_along instead of 1:length
+  var <- highlight_vars[i]
+  col <- highlight_colors[i]
+  
+  lines(mtcars[, var], col = col, lwd = 2)
+  points(mtcars[, var], col = col, pch = 16)
 }
