e15afc8a224484d9fa8a29971e9f4dd665f89b46
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-09-10 19:06:02 +0530

    Update Q4.R

--- Q4.R
+++ Q4.R
@@ -1,3 +1,5 @@
+#a
+#Either increment from 0 to 2 or 1 to 3
 for (a in 0:2) {
   for (b in 0:2) {
     if (b == 2) {
@@ -6,3 +8,26 @@ for (a in 0:2) {
     cat("a:", a, ", b:", b, "\n")
   }
 }
+
+#b
+#skipping 3 and 4
+i <- 2
+while (i <= 5) {
+  if (i == 3 || i == 4) {
+    i <- i + 1
+    next
+  }
+  cat(i, "\n")
+  i <- i + 1
+}
+
+#skipping only one number
+# i <- 2
+# while (i <= 5) {
+#   if (i == 4) {
+#     i <- i + 1
+#     next
+#   }
+#   cat(i, "\n")
+#   i <- i + 1
+# }
