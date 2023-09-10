54be30c8cf843ac6d6582f52483c833ed340a7e1
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-09-10 23:52:43 +0530

    Update Q1.R

--- Q1.R
+++ Q1.R
@@ -3,13 +3,13 @@ A <- c(1, 2, 3, 4, 5)
 B <- c(6, 7, 8, 9, 10)
 
 # Arithmetic operations on vectors A and B
-sum_A_B <- A + B # Addition
+sum <- A + B # Addition
 diff_A_B <- A - B # Subtraction
 prod_A_B <- A * B # Element-wise multiplication
 div_A_B <- A / B # Element-wise division
 pow_A_B <- A ^ B # Element-wise exponentiation
 # Print the results
-cat("Sum of A and B:", sum_A_B, "\n")
+cat("Sum of A and B:", sum, "\n")
 cat("Difference of A and B:", diff_A_B, "\n")
 cat("Product of A and B:", prod_A_B, "\n")
 cat("Division of A and B:", div_A_B, "\n")
@@ -17,10 +17,10 @@ cat("Exponentiation of A and B:", pow_A_B, "\n")
 
 # Apply rep(), paste(), and cumprod() functions on vector A
 rep_A <- rep(A, times = 2) # Repeat A twice
-paste_A <- paste("A: ",A, sep = " ") # Concatenate "A:" with each element of A
+paste_A_B <- paste(A, sep = "-", B) # Concatenate "A:" with each element of A
 cumprod_A <- cumprod(A) # Cumulative product of A
 
 # Print the results
 cat("Repeated A:", rep_A, "\n")
-cat("Pasted Values:", paste_A, "\n")
+cat("Pasted  A and B together :", paste_A_B, "\n")
 cat("Cumulative product of A:", cumprod_A, "\n")
