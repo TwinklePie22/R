c05cb21392afb66d45200e8bff4c081a968d73bb
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-09-11 00:23:19 +0530

    Update Q5.R

--- Q5.R
+++ Q5.R
@@ -3,14 +3,14 @@ count_divisors <- function(num) {
   count <- 0       # To keep track of the count of divisors
   
   cat("Given Integer:", num, "\n")
-  for (i in 2:(num - 1)) {
+  for (i in 1:(num )) {
     if (num %% i == 0) {
       divisors <- c(divisors, i)
       count <- count + 1
     }
   }
   cat("Divisors:", divisors, "\n")
-  cat("Number of divisors (excluding 1 and itself):", count, "\n")
+  cat("Number of divisors (excluding 1 and itself):", count -2, "\n")
 }
 
 # Example usage:
